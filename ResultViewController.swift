5cc1b35f6ca231c639d152b2a392add1e1238afe
asa10yoru <asa10yoru@techacademy.jp>
2017-03-04 20:26:26 +0900

    ボタンの遷移まで

---
+++ ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by ぽんじゅうす on 2017/03/04.
+//  Copyright © 2017年 asa10yoru. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+}
